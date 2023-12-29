ca452a0497b8f46629a772e43c8b689f25590655
ParanoidBat <cool_omer@hotmail.co.uk>
2023-12-30 01:47:07 +0500

    minor change

--- src/components/ActionButton/styles.js
+++ src/components/ActionButton/styles.js
@@ -1,11 +1,10 @@
 export default {
-    button: {
-        height: 62,
-        padding: "12px 24px",
-        borderRadius: 50,
-        background: '#e74c1b',
-        color: '#fff',
-        fontSize: 25,
-        border: 'none'
-    }
-}
+  button: {
+    padding: "12px 24px",
+    borderRadius: 50,
+    background: "#e74c1b",
+    color: "#fff",
+    fontSize: 25,
+    border: "none",
+  },
+};
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,38 +1,38 @@
 export default {
-    container: {
-        background: "#f0f4fc", 
-        borderRadius: 50, 
-        padding: "18px 30px"
-    },
-    heading: {
-        color: "#000",
-        fontSize: "25px",
-        fontWeight: "600",
-    },
-    subHeading: {
-        color: "#000",
-        fontSize: "16px",
-        fontWeight: "500",
-    },
-    category: {
-        borderRadius: 10,
-        width: 145,
-    },
-    item: {
-        borderRadius: "20px 20px 0px 0px",
-        objectFit: "cover",
-        width: 140,
-    },
-    info: {
-        padding: "8px 13px",
-        width: 140,
-        borderRadius: "0px 0px 20px 20px",
-        boxShadow: "0px 2px 20px 0px rgba(0,0,0,0.05)",
-        background: "#fff"
-    },
-    infoText: {
-        color: "#000",
-        fontSize: 14,
-        fontWeight: 500
-    },
-}
+  container: {
+    background: "#f0f4fc",
+    borderRadius: 50,
+    padding: "18px 30px",
+  },
+  heading: {
+    color: "#000",
+    fontSize: "25px",
+    fontWeight: "600",
+  },
+  subHeading: {
+    color: "#000",
+    fontSize: "16px",
+    fontWeight: "500",
+  },
+  category: {
+    borderRadius: 10,
+    width: 145,
+  },
+  item: {
+    borderRadius: "20px 20px 0px 0px",
+    objectFit: "cover",
+    width: 140,
+  },
+  info: {
+    padding: "8px 13px",
+    width: 140,
+    borderRadius: "0px 0px 20px 20px",
+    boxShadow: "0px 2px 20px 0px rgba(0,0,0,0.05)",
+    background: "#fff",
+  },
+  infoText: {
+    color: "#000",
+    fontSize: 14,
+    fontWeight: 500,
+  },
+};
