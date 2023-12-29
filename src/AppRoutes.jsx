e610fa76f46f5fb26a71aefab4030f51febdc19c
ParanoidBat <cool_omer@hotmail.co.uk>
2023-12-29 21:06:25 +0500

    create checkout page

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Product from "./pages/Product";
 import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
 
 export default function AppRoutes() {
     return (
@@ -9,6 +10,7 @@ export default function AppRoutes() {
             <Route path="/" element={<Home/>} />
             <Route path="/product" element={<Product />} />
             <Route path="/cart" element={<Cart/>} />
+            <Route path="/checkout" element={<Checkout />} />
         </Routes>
     )
 }
---
+++ src/pages/Checkout/styles.js
@@ -0,0 +1,28 @@
+export default {
+    container: {
+        background: '#f0f4fc',
+        borderRadius: 50
+    },
+    heading: {
+        fontSize: 25,
+        fontWeight: 600,
+        color: '#000',
+    },
+    paymentinfo: {
+        color: '#000',
+        fontSize: 14,
+        fontWeight: 500,
+    },
+    radioGroup: {
+        "& > label": {
+            marginBottom: 5
+        },
+        '.Mui-checked': {
+            color: "black"
+        }
+    },
+    details: {
+        color: "#a1a1a1",
+        fontSize: 16
+    }
+}
